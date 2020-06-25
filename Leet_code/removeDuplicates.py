f5507276607187c998ff7f2fc1270d3611cc5e0f
PrabhuRakshitha <rakshitha.p45@gmail.com>
2020-06-25 12:06:08 -0700

    updating files

--- Leet_code/removeDuplicates.py
+++ Leet_code/removeDuplicates.py
@@ -16,6 +16,18 @@ class Solution(object):
                 i = i+1
         return nums
 
+#Remove Duplicates from Sorted Array II
+# Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
+#
+# Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+class Solution2(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+
 
 if __name__ =="__main__":
     obj = Solution()
